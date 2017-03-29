35b5f42c3e979435ff2c21b59595baacf5a9e03b
Aleksefo <aleksefo@gmail.com>
2017-03-29 23:47:31 +0300

    Menu added

--- app/src/main/java/com/example/alex/nanopopularmovies/MainActivity.java
+++ app/src/main/java/com/example/alex/nanopopularmovies/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.alex.nanopopularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -9,18 +13,47 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.button)
-  Button button;
+	@BindView(R.id.button)
+	Button button;
+	private static final String TAG = "MainActivity";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate: start");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+		Log.d(TAG, "onCreate: end");
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
+		return true;
+	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+//		switch (item.getItemId()) {
+//			case R.id.new_game:
+//				newGame();
+//				return true;
+//			case R.id.help:
+//				showHelp();
+//				return true;
+//			default:
+//				return super.onOptionsItemSelected(item);
+//		}
+//	}
 
-  @OnClick(R.id.button)
-  public void onViewClicked() {
+	@OnClick(R.id.button)
+	public void onViewClicked() {
+		Log.d(TAG, "onViewClicked: ");
+	}
 
-  }
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
